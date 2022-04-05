9631d67a3f42c258c93e960f3ee7e4c8b446045b
Jan <33833587+Model2TypeE@users.noreply.github.com>
2022-04-05 07:48:18 +0200

    initialized project settings

--- CUDA/FFT/src/Entry.cu
+++ CUDA/FFT/src/Entry.cu
@@ -1,9 +1,7 @@
+
+
 int main()
 {
-    const int arraySize = 5;
-    const int a[arraySize] = { 1, 2, 3, 4, 5 };
-    const int b[arraySize] = { 10, 20, 30, 40, 50 };
-    int c[arraySize] = { 0 };
 
     return 0;
 }
